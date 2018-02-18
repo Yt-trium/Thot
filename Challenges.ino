b2c089e866cb46612c4b1a8b77e172de0f9955d2
Yt-trium <cyril.meyer.68@gmail.com>
2018-02-18 21:08:13 +0100

    add comment and challenges definition

---
+++ Challenges.ino
@@ -0,0 +1,30 @@
+// Challenges : The list of your challenges password
+// challengesSize[i][j] is the size of the challenge number i part j
+// challenges[i][j] is the password string of the challenge number i part j
+
+const byte challengesSize[9][2] =
+                            {{4,3}
+                            ,{4,0}
+                            ,{0,0}
+                            ,{0,0}
+                            ,{0,0}
+                            ,{0,0}
+                            ,{0,0}
+                            ,{0,0}
+                            ,{0,0}
+                            };
+
+
+
+const char challenges[9][2][8] =
+                            {{{'1','2','3','4'},{'7','8','9'}}
+                            ,{{'4','3','2','1'},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            ,{{0,0},{0,0}}
+                            };
+
--- Thot.ino
+++ Thot.ino
@@ -1,8 +1,27 @@
-// Thot
+/*
+ * Thot
+ * 
+ * 4 RGB Leds (with 3 states)
+ * Red    (nothing validated)
+ * Orange (1/2 validated)
+ * Green  (validated)
+ * 
+ * 1 Keypad
+ * Assigned Keys :
+ * '*' -> Switch page (you can use up to 255 pages, but we only use 2)
+ * '#' -> Validation
+ * 
+ * 1 MFRC522 13.56Mhz RFID Reader
+ * 
+ * 1 SSD1306 Display
+ */
 
 #include <Keypad.h>
-
 extern Keypad keypad;
+
+
+extern const byte challengesSize[9][2];
+extern const char challenges[9][2][8];
 
 void setup()
 {
